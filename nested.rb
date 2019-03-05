e1c003ec3bb8db1973edec8a714a6d53ab1beade
Audra Tallman <audra.tallman@hotmail.com>
2019-03-05 05:17:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-return programmer_hash[:dennis_ritchie][:languages]
+return programmer_hash[:dennis_ritchie][:languages].first
 end
 
 def adding_matz
@@ -80,8 +80,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash [:yukihiro_matsumoto]
-programmer_hash [:known_for] = "Ruby" [:languages] = "LISP", "C"
+programmer_hash [:yukihiro_matsumoto] = {"known_for" => "Ruby", "languages" => "LISP", "C"}
 end
 
 def changing_alan
