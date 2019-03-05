c95d94552c5c343f76519811a5aa072df812cbfb
Audra Tallman <audra.tallman@hotmail.com>
2019-03-05 05:03:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -35,7 +35,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-return programmer_hash [:alan_kay][:known_for]    
+return programmer_hash [:alan_kay][:known_for].to_s
+
 end
 
 def dennis_ritchies_language
